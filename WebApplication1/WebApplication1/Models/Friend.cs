2f6f59f28d323653d3c173da4c21d3499231e336
Adan Ardaya Dominguez <a2017118141@estudiantes.upsa.edu.bo>
2021-04-24 10:52:15 -0400

    Modelo creado

---
+++ WebApplication1/WebApplication1/Models/Friend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Friend
+    {
+        [Key]
+        
+        public string FrienId { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [Required]
+        public int NikName { get; set; }
+        [Required]
+        public DataType Birthday { get; set; }
+    }
+}
